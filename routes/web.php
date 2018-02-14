65e7c8929a54b3f863942d8143a44a68b84eea19
zap-k-namikawa <k_namikawa@zappallas.com>
2018-02-14 15:37:45 +0900

    とりあえずpush

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('articles/create', 'ArticlesController@create');
 Route::get('articles/{id}', 'ArticlesController@show');
 Route::post('articles', 'ArticlesController@store');
 Route::get('articles/pop', 'ArticlesController@pop1');
+Route::get('articles/{id}/edit', 'ArticlesController@edit');  // 追加
+Route::patch('articles/{id}', 'ArticlesController@update');  // 追加
+Route::delete('articles/{id}', 'ArticlesController@destroy'); 
